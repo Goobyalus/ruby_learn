4435e55fd05c55b8a0353ed72276a66020cb1352
Goobyalus <jspurch@gmail.com>
2014-08-13 11:33:30 -0400

    rdp progress

--- rdp.rb
+++ rdp.rb
@@ -8,4 +8,17 @@ DIGIT-> /[0-9]/
 =end
 
 
+def parse_wchar(str, chrs)
+	if chrs.include? str[0]
+		return [str[0], str[1...str.length]]
+	raise "No [#{chrs}] to parse in parse_wchar"
+end
 
+def parse_digit(str)
+	return parse_wchar(str, range('0'..'9'))
+end
+
+def parse_prod(str)
+	digit = parse_digit(str)
+	
+end

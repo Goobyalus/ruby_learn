288ae88bed6366ef65bd2ce4d6c959e6bd10f6e5
Goobyalus <jspurch@gmail.com>
2014-08-16 00:41:15 -0400

    finished dijkstra copied to repo

--- dijkstra.rb
+++ dijkstra.rb
@@ -1,29 +1,40 @@
-require 'priority_queue'
 
-q = PriorityQueue.new
-nodes = {}
-visited = {}
-unvisited = {}
-dist = {}
 
+
+#graph represented as hash{string => hash{string => ?}}
 
 
 def dijkstra(graph, source)
+	dist = {}
+	dist[source] = 0
+	
+	queue = [source]
+	prev = {}
+	
+	for v in (graph.keys)-queue
+		dist[v] = Float::INFINITY
+		prev[v] = nil
+		queue.push(v)
+	end
 
-	#initialize distances to infinity
-	for node in graph
-	
-		if node == source
-			dist[node] = 0.0
-		else
-			dist[node] = Float::Infiniity
+	while not queue.empty?
+		queue.sort_by!{|o| dist[o]}
+		u = queue.delete_at(0)
+		
+		for v in graph[u].keys
+			alt = dist[u] + graph[u][v]
+			if nil == alt then alt = Float::INFINITY end
+			
+			if alt < dist[v]
+				dist[v] = alt
+				prev[v] = u
+			end
 		end
-	
-		q[node] = dist[node]
 	end
 	
-	#bf
-	while 
-	
+	return [dist,prev]
 
 end
+
+print "\n"
+print dijkstra({1=>{2=>1, 3=>2}, 2=>{3=>2, 4=>1}, 3=>{}, 4=>{}, 5=>{}}, 1)
